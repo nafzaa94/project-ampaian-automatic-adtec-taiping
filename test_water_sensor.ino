6df42bc4f71b921dcecd042aa236d3b256dc61cd
nafzaa94 <78883615+nafzaa94@users.noreply.github.com>
2022-06-14 22:36:44 +0800

    Update test_water_sensor.ino

--- test_water_sensor.ino
+++ test_water_sensor.ino
@@ -1,10 +1,13 @@
 int watersensor = 34;
+int relay = 35;
 int valuewatersensor = 0;
 
 void setup() {
   Serial.begin(9600);
   pinMode(watersensor, INPUT);
+  pinMode(relay, OUTPUT);
 
+  digitalWrite(relay, HIGH);
 }
 
 void loop() {
