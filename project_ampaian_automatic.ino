3e526ce9714b549547db23f494a5aeb1226ea868
nafzaa94 <78883615+nafzaa94@users.noreply.github.com>
2022-06-21 22:53:37 +0800

    Update project_ampaian_automatic.ino

--- project_ampaian_automatic.ino
+++ project_ampaian_automatic.ino
@@ -1,15 +1,14 @@
-#define BLYNK_TEMPLATE_ID "TMPLc_e0uRfE"
-#define BLYNK_DEVICE_NAME "test3"
-#define BLYNK_AUTH_TOKEN "uIlk6_tEDbcnCXvybsryJG_lUfvIVp9w"
+#define BLYNK_TEMPLATE_ID           "TMPLxxxxxx"
+#define BLYNK_DEVICE_NAME           "Device"
+#define BLYNK_AUTH_TOKEN            "YourAuthToken"
 
 
 // Comment this out to disable prints and save space
 #define BLYNK_PRINT Serial
 
 
-#include <WiFi.h>
-#include <WiFiClient.h>
-#include <BlynkSimpleEsp32.h>
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
 
 char auth[] = BLYNK_AUTH_TOKEN;
 
@@ -18,10 +17,10 @@ char auth[] = BLYNK_AUTH_TOKEN;
 char ssid[] = "YourNetworkName";
 char pass[] = "YourPassword";
 
-int watersensor = 34;
+int watersensor = A0;
 int valuewatersensor = 0;
 
-int relay = 35;
+int relay = D2;
 bool statewatersensor = false;
 
 BlynkTimer timer;
@@ -41,32 +40,36 @@ BLYNK_WRITE(V1)
 
 void myTimerEvent()
 {
-  valuewatersensor = digitalRead(watersensor);
+  valuewatersensor = analogRead(watersensor);
 
-  if(valuewatersensor == LOW && statewatersensor == false){
+  if(valuewatersensor <= 500 && statewatersensor == false){
     digitalWrite(relay, LOW);
     statewatersensor = true;
     Blynk.logEvent("notification", "hujan.....");
   }
 
-  else if(valuewatersensor == HIGH && statewatersensor == true){
+  else if(valuewatersensor >= 500 && statewatersensor == true){
     digitalWrite(relay, HIGH);
     statewatersensor = false;
     }
-  
 }
 
 void setup()
 {
   // Debug console
   Serial.begin(115200);
+
   pinMode(watersensor, INPUT);
   pinMode(relay, OUTPUT);
 
   digitalWrite(relay, HIGH);
 
   Blynk.begin(auth, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);
+  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
 
+  // Setup a function to be called every second
   timer.setInterval(1000L, myTimerEvent);
 }
 
